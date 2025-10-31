866573a5d822a1e678f6c145be3150d2410cb1bd
meryemhamidi <meryam@gmail.com>
2025-10-31 22:39:34 +0100

    Creation de la classe Test1

---
+++ src/main/java/ma/emsi/hamidi/Test1.java
@@ -0,0 +1,30 @@
+package ma.emsi.hamidi;
+
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
+
+import java.util.Scanner;
+
+public class Test1 {
+    public static void main(String[] args) {
+        String cle = System.getenv("GEMINI_API_KEY");
+
+        ChatModel model = GoogleAiGeminiChatModel
+                .builder()
+                .apiKey(cle)
+                .modelName("gemini-2.5-flash")
+                .temperature(0.7)
+                .build();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Veuillez saisir votre qst: ");
+        String question = scanner.nextLine();
+
+        String reponse = model.chat(question);
+
+        System.out.println("=====================================================");
+        System.out.println("Réponse du modèle : " + reponse);
+        System.out.println("=====================================================");
+
+    }
+}
